f7b9f0d29a5624d952274ffe702d06a04c6a6dd7
Cris Daniluk <cdaniluk@rhythmictech.com>
2020-05-19 13:00:02 -0400

    Add output (#4)
    * bump versions
    
    * add output for bucket domain

--- outputs.tf
+++ outputs.tf
@@ -3,8 +3,12 @@ output "s3logging_bucket_arn" {
   value       = aws_s3_bucket.s3logging_bucket.arn
 }
 
+output "s3logging_bucket_domain_name" {
+  description = "The domain name of the bucket"
+  value       = aws_s3_bucket.s3logging_bucket.bucket_domain_name
+}
+
 output "s3logging_bucket_name" {
   description = "The name of the bucket"
   value       = aws_s3_bucket.s3logging_bucket.bucket
 }
-
